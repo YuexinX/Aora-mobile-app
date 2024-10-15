c7fa074af787c794115f590d7246d401bc0a5aed
Yuexin Xu <singledogxu@gmail.com>
2024-10-14 20:19:25 -0700

    setting up project

---
+++ app/(auth)/sign-in.jsx
@@ -0,0 +1,26 @@
+import { View, Text, ScrollView, Image } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { images } from '../../constants'
+import FormField from '../../components/FormField'
+
+const SignIn = () => {
+  return (
+    <SafeAreaView className = "bg-primary h-full">
+      <ScrollView>
+        <View className="w-full justify-center h-full px-4 my-6">
+          <Image source={images.logo} resigMode='contain' className='w-[115px] h-[35px]' />
+          <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">
+            Log in to Aora
+          </Text>
+          <FormField 
+            title = "Email"
+          />
+        </View>
+      </ScrollView>
+
+    </SafeAreaView>
+  )
+}
+
+export default SignIn
